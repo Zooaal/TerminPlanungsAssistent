c28823e79c8ad8fa67c5950fce768bc04c70f917
Zooaal <inf19133@lehre.dhbw-stuttgart.de>
2021-03-22 11:19:57 +0100

    Projektdateien hinzufügen.

---
+++ DataAccessLibary/MongoCRUD.cs
@@ -0,0 +1,64 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibary
+{
+    public class MongoCRUD
+    {
+        private IMongoDatabase db;
+        public MongoCRUD(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public T LoadRecordByEmail<T>(string table, string email)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Email", email);
+
+            return collection.Find(filter).First();
+        }
+
+        public T LoadRecordById<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("ID", id);
+
+            return collection.Find(filter).First();
+        }
+     
+        public void UpsertRecord<T>(string table, Guid id, T record)
+        {
+            var collection = db.GetCollection<T>(table);
+
+            var result = collection.ReplaceOne(
+                new BsonDocument("_id", id),
+                record,
+                new ReplaceOptions { IsUpsert = true });
+        }
+
+        public void DeleteRecord<T>(string table, Guid id)
+        {
+            var collection = db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("ID", id);
+            collection.DeleteOne(filter);
+        }
+    }
+}
---
+++ TerminPlanungsAssistent/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TerminPlanungsAssistent.Areas.Identity.Data;
+using TerminPlanungsAssistent.Data;
+
+[assembly: HostingStartup(typeof(TerminPlanungsAssistent.Areas.Identity.IdentityHostingStartup))]
+namespace TerminPlanungsAssistent.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TerminPlanungsAssistentContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TerminPlanungsAssistentContextConnection")));
+
+                services.AddDefaultIdentity<TerminPlanungsAssistentUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TerminPlanungsAssistentContext>();
+            });
+        }
+    }
+}
